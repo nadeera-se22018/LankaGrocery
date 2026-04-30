517ff1e3d1c44f2ef733e7f18a2224e6ebd8d599
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-04-30 11:38:08 +0530

    feat: Import & Connect productRoutes to the app.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('LankaGrocery API is running...');
 });
+
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
