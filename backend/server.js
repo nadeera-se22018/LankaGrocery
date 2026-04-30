dcaf3f429384adbd04c2054b138cf67ad7dfea75
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-04-30 16:10:57 +0530

    feat: Import & Connect notFound & errorHandler Error middlewares to the app.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -18,6 +19,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
