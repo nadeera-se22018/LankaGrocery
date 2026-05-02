566a0eafcdf8890173b52b7c72e73c7eb6caba2c
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-02 15:17:10 +0530

    feat: Write Layout Components for Footer.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4 mt-auto">
+      <p>&copy; {new Date().getFullYear()} LankaGrocery. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
