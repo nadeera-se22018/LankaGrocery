61c8a59b942cb0570d88e2911fab6345155210b1
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-04 11:02:39 +0530

    feat: Add ProductPage Route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <main className="flex-grow container mx-auto px-4 py-6">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
           </Routes>
         </main>
         <Footer />
