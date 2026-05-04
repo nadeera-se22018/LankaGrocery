4bc5204044bfbb5c9f07381ab761086096a39ec8
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-04 10:23:59 +0530

    feat: Fetch Data into the Home Page.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Product from "../components/Product";
+
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/api/products");
+        
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Latest Products</h1>
-      <p className="text-gray-600">Products will be displayed here soon...</p>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold mb-6">Latest Products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </>
   );
 };
 
