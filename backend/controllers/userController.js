bb1ccc30f8d397958979733989418e7d4de149cb
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-05 11:54:25 +0530

    feat: Craete User Controller and add main function for login

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
+
+export const authUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (user && (await user.matchPassword(password))) {
+            generateToken(res, user._id); 
+
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                avatar: user.avatar
+            });
+        } else {
+            res.status(401).json({ message: 'Waradi Email ekak hari Password ekak hari deela thiyenne' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
