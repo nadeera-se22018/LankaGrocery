d97c3677ecde4195dd3f2230a1dad6e7c99247cb
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-05 11:59:53 +0530

    feat: Update server.js and import Cookie Parser & User Routes.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -12,13 +14,15 @@ connectDB();
 const app = express();
 
 app.use(cors()); 
-app.use(express.json()); 
+app.use(express.json());
+app.use(cookieParser()); 
 
 app.get('/', (req, res) => {
     res.send('LankaGrocery API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
