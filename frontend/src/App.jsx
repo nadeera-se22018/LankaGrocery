619f64bec870526fc097a121cf066d910e294aff
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-06 17:17:36 +0530

    feat: Import RegisterPage, PrivateRoute, ShippingPage, & update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PrivateRoute from './components/PrivateRoute';
+import ShippingPage from './pages/ShippingPage';
 
 const App = () => {
   return (
@@ -21,6 +24,12 @@ const App = () => {
             <Route path="/product/:id" element={<ProductPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+
+            <Route path="" element={<PrivateRoute />}>
+              <Route path="/shipping" element={<ShippingPage />} />
+            </Route>
+            
           </Routes>
         </main>
         <Footer />
