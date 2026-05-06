233a91697d46fad4afbd71afdceb778c4983b557
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-06 16:58:48 +0530

    feat: Add authUser & registerUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,3 +24,39 @@ export const authUser = async (req, res) => {
         res.status(500).json({ message: 'Server Error', error: error.message });
     }
 };
+
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            res.status(400).json({ message: 'An account has been created with this email before' });
+            return;
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password, 
+        });
+
+        if (user) {
+            generateToken(res, user._id);
+
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                avatar: user.avatar
+            });
+        } else {
+            res.status(400).json({ message: 'Invalid user data' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
