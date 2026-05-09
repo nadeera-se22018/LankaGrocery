c40012cde80c4b69ed2de353100083daf968f0e3
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-09 10:05:23 +0530

    feat: Update userController by adding updateUserProfile function.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -60,3 +60,33 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: 'Server Error', error: error.message });
     }
 };
+
+
+export const updateUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+
+        if (user) {
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+
+            if (req.body.password) {
+                user.password = req.body.password; 
+            }
+
+            const updatedUser = await user.save();
+
+            res.status(200).json({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                isAdmin: updatedUser.isAdmin,
+                avatar: updatedUser.avatar,
+            });
+        } else {
+            res.status(404).json({ message: 'Could not find user.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Could not update the profile.', error: error.message });
+    }
+};
