fdd0e2d9cef2fb789971fc0fc7e2b1dc4760d0c4
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-09 10:09:30 +0530

    feat: Import new 'protect' middleware and send PUT request to update the profile.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { authUser, registerUser } from '../controllers/userController.js';
+import { authUser, registerUser, updateUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(registerUser);
 
 router.post('/login', authUser);
+
+router.route('/profile').put(protect, updateUserProfile);
 
 export default router;
