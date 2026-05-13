4f545131dd671833e24bfbe91c11e5d633f083c3
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-13 18:18:18 +0530

    feat: Update product controller and add createProduct & deleteProduct functions.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,3 +22,39 @@ export const getProductById = async (req, res) => {
         res.status(500).json({ message: 'Server Error because ID format is wrong', error: error.message });
     }
 };
+
+export const createProduct = async (req, res) => {
+    try {
+        const product = new Product({
+            name: 'Sample Product Name',
+            price: 0,
+            user: req.user._id,
+            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+            brand: 'Sample Brand',
+            category: 'Sample Category',
+            countInStock: 0,
+            numReviews: 0,
+            description: 'Sample description for the new product...',
+        });
+
+        const createdProduct = await product.save();
+        res.status(201).json(createdProduct);
+    } catch (error) {
+        res.status(500).json({ message: 'Could not create the product', error: error.message });
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (product) {
+            await Product.deleteOne({ _id: product._id });
+            res.status(200).json({ message: 'Delete the product' });
+        } else {
+            res.status(404).json({ message: 'Could not find the product' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error, Could not delete', error: error.message });
+    }
+};
