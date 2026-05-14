152afd394096ee3dadec0d188cc787504c0ed386
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-14 20:36:49 +0530

    feat: Update product controller and add updateProduct function.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -58,3 +58,28 @@ export const deleteProduct = async (req, res) => {
         res.status(500).json({ message: 'Server Error, Could not delete', error: error.message });
     }
 };
+
+export const updateProduct = async (req, res) => {
+    try {
+        const { name, price, description, image, brand, category, countInStock } = req.body;
+
+        const product = await Product.findById(req.params.id);
+
+        if (product) {
+            product.name = name;
+            product.price = price;
+            product.description = description;
+            product.image = image; 
+            product.brand = brand;
+            product.category = category;
+            product.countInStock = countInStock;
+
+            const updatedProduct = await product.save();
+            res.status(200).json(updatedProduct);
+        } else {
+            res.status(404).json({ message: 'Could not find the product' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Could not update the product', error: error.message });
+    }
+};
