408779fb7c061a70f23e03134f3fc1f798d6753b
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-14 21:54:30 +0530

    feat: Import getUsers & deleteUser functions and connect them to routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { authUser, registerUser, updateUserProfile } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { authUser, registerUser, updateUserProfile, getUsers, deleteUser } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 
 router.post('/login', authUser);
 
 router.route('/profile').put(protect, updateUserProfile);
+
+router.route('/:id').delete(protect, admin, deleteUser);
 
 export default router;
