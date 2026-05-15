f5a4f81526b2d501fa6515fb6cfa8f9cf9a231fa
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-15 19:33:49 +0530

    feat: Import createProductReview and connect it to the routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getProducts, getProductById, createProduct, deleteProduct, updateProduct } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct, deleteProduct, updateProduct, createProductReview } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+
+router.route('/:id/reviews').post(protect, createProductReview);
 
 router.route('/:id')
     .get(getProductById)
