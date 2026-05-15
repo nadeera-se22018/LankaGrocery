b0920dc248168221c249f1d795025585cec34cff
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-15 19:30:33 +0530

    feat: Write the logic to add the backend's review to database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -88,3 +88,41 @@ export const updateProduct = async (req, res) => {
         res.status(500).json({ message: 'Could not update the product', error: error.message });
     }
 };
+
+export const createProductReview = async (req, res) => {
+    try {
+        const { rating, comment } = req.body;
+
+        const product = await Product.findById(req.params.id);
+
+        if (product) {
+            const alreadyReviewed = product.reviews.find(
+                (r) => r.user.toString() === req.user._id.toString()
+            );
+
+            if (alreadyReviewed) {
+                res.status(400).json({ message: 'Already you reviewed this product' });
+                return;
+            }
+
+            const review = {
+                name: req.user.name,
+                rating: Number(rating),
+                comment,
+                user: req.user._id,
+            };
+
+            product.reviews.push(review);
+
+            product.numReviews = product.reviews.length;
+            product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+            await product.save();
+            res.status(201).json({ message: 'Successfully added your review' });
+        } else {
+            res.status(404).json({ message: 'Could not find product' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Could not save the review', error: error.message });
+    }
+};
