f372f67cfcab8ac4ad9075eac6bab7b4817b30fd
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-18 12:40:21 +0530

    feat: Import getTopProducts function and add it to the API routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getProducts, getProductById, createProduct, deleteProduct, updateProduct, createProductReview } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct, deleteProduct, updateProduct, createProductReview, getTopProducts } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 router.route('/:id/reviews').post(protect, createProductReview);
+
+router.get('/top', getTopProducts);
 
 router.route('/:id')
     .get(getProductById)
