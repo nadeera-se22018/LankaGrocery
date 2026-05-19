68b2ba28a511853615194c1a6b8d43d5e012897b
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-19 19:05:57 +0530

    feat: Import HelmetProvider & Wrap the app using it

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'; 
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PayPalScriptProvider deferLoading={true}>
-      <App />
-    </PayPalScriptProvider>
+    <HelmetProvider>
+      <PayPalScriptProvider deferLoading={true}>
+        <App />
+      </PayPalScriptProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 );
 
