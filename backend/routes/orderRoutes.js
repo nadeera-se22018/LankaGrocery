f3745b4b391be6ead1b2a5b58716e73713f29481
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-19 11:53:22 +0530

    feat: Import updateOrderToPaid and connect it to routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addOrderItems, getOrderById, getMyOrders, getOrders, updateOrderToDelivered } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById, getMyOrders, getOrders, updateOrderToDelivered, updateOrderToPaid } from '../controllers/orderController.js';
 import { protect, admin } from '../middleware/authMiddleware.js'; 
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
