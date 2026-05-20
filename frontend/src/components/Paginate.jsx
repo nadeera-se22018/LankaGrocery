253f871b49c9b8bdbce4e224e67e4fa7bb3c954e
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-20 14:41:48 +0530

    feat: Create pagination for category filterize multiple pages

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,30 +1,30 @@
 import { Link } from 'react-router-dom';
 
-const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+const Paginate = ({ pages, page, keyword = '', category = '' }) => {
+  if (pages <= 1) return null;
+
   return (
-    pages > 1 && (
-      <div className="flex justify-center mt-8 space-x-2">
-        {[...Array(pages).keys()].map((x) => (
+    <div className="flex justify-center mt-10 space-x-2">
+      {[...Array(pages).keys()].map((x) => {
+        let link = `/page/${x + 1}`;
+        if (keyword) link = `/search/${keyword}/page/${x + 1}`;
+        if (category) link = `/category/${category}/page/${x + 1}`;
+
+        return (
           <Link
             key={x + 1}
-            to={
-              !isAdmin
-                ? keyword
-                  ? `/search/${keyword}/page/${x + 1}`
-                  : `/page/${x + 1}`
-                : `/admin/products/${x + 1}`
-            }
-            className={`px-4 py-2 rounded font-bold transition ${
+            to={link}
+            className={`px-4 py-2 border rounded-md transition font-bold shadow-sm ${
               x + 1 === page
-                ? 'bg-green-600 text-white shadow-md'
-                : 'bg-gray-200 text-gray-700 hover:bg-gray-300'
+                ? 'bg-green-600 text-white border-green-600'
+                : 'bg-white text-green-600 border-gray-200 hover:bg-green-50/80'
             }`}
           >
             {x + 1}
           </Link>
-        ))}
-      </div>
-    )
+        );
+      })}
+    </div>
   );
 };
 
