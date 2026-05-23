1aa8317da4047d12011ba4e5aab36185b98c8fae
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-23 19:21:39 +0530

    chore: Add origin & credentials to cors

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser()); 
-
-app.get('/', (req, res) => {
-    res.send('LankaGrocery API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -44,5 +44,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running in development mode on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
