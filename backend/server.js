4f16866047610156a47feac35489c34494b525d4
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-23 21:58:45 +0530

    feat: Import and add CORS Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -17,11 +18,12 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', 
+  credentials: true, 
 }));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); 
 
 app.use('/api/products', productRoutes);
