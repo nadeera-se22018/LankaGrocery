364c06021fc7671483a151eb188ed2cba1079a65
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-05-23 20:32:13 +0530

    feat: Import GoogleOAuthProvider and clientId

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx';
 import './index.css';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'; 
 import { HelmetProvider } from 'react-helmet-async';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <PayPalScriptProvider deferLoading={true}>
-        <App />
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <App />
+        </GoogleOAuthProvider>
       </PayPalScriptProvider>
     </HelmetProvider>
   </React.StrictMode>,
